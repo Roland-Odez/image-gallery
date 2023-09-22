9af0ad56afaa96b7fc309acc96bdffc08058deda
Roland Odenore - jsboy <breezyroland@gmail.com>
2023-09-22 12:51:58 +0100

    perfected dnd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,21 @@
 
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { imageData } from '../lib/images'
-import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core'
-import { SortableContext, rectSortingStrategy } from '@dnd-kit/sortable'
 import { app } from '../firebase/config'
 import { useNavigate } from 'react-router-dom'
 import ImageContainer from '../components/ImageContainer'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createRef } from 'react'
 import { ImageType } from '../types/imageTypes'
 import Navbar from '../components/Navbar'
+import Sortable from "sortablejs";
 
 
 export default function Home() {
     const [images, setImages] = useState<ImageType[]>([...imageData])
-    const [show, setShow] = useState<boolean>(false)
+    const [show, setShow] = useState<boolean>(true)
     const navigate = useNavigate()
     const auth = getAuth(app);
+    const sortableContainer = createRef<HTMLElement>()
 
     onAuthStateChanged(auth, (user) => {
         if (!user) {
@@ -46,43 +46,53 @@ export default function Home() {
 
     }
 
-    const onDragEnd = (e: DragEndEvent) => {
-        // console.log({
-        //     s: e.active?.data.current?.sortable.index,
-        //     ed: e.over?.data.current?.sortable.index
-        // })
+
+    const onDragEnd = (e: Sortable.SortableEvent) => {
+
         const imageItems = [...images]
-        const dragedImage = imageItems.splice(Number(e.active?.data.current?.sortable.index) - 1, 1)[0]
-        imageItems.splice(Number(e.over?.data.current?.sortable.index) - 1, 0, dragedImage)
+        const [dragedImage] = imageItems.splice(Number(e.oldIndex), 1)
+        imageItems.splice(Number(e.newIndex), 0, dragedImage)
         setImages(imageItems)
     }
 
     useEffect(() => {
+        const container: HTMLElement | any = sortableContainer.current
+        Sortable.create(container, {
+            animation: 550,
+            onEnd: onDragEnd
+        })
+        // if (mount) {
+        //     console.log('swap is called ................')
+        //     setMount(false)
+        // }
+        // Sortable.mount(new Swap());
+
+    }, [])
+
+    useEffect(() => {
         setTimeout(() => {
-            setShow(true)
+            setShow(false)
         }, 3000)
     }, [])
 
     return (
         <>
             <Navbar handleSearch={handleSearch} />
-            <main className="lg:overflow-y-auto h-screen pb-20 max-w-[1124px] mx-auto no-scrollbar">
-                {
-                    show ?
+            <main className="overflow-y-auto h-screen pb-20 max-w-[1124px] mx-auto no-scrollbar">
                         <section
-                            className='px-3 py-4 grid grid-cols-2  md:grid-cols-custom gap-4 no-scrollbar'>
-                            <DndContext collisionDetection={closestCenter} onDragEnd={(e) => onDragEnd(e)}>
-                                <SortableContext items={images} strategy={rectSortingStrategy}>
+                    ref={sortableContainer}
+                    className='px-3 py-4 grid  grid-cols-custom gap-4 no-scrollbar'>
+
                                     {
                                         images.map((image) => (
                                             <ImageContainer key={image?.id} image={image} />
 
                                         ))}
-                                </SortableContext>
-                            </DndContext>
+
                         </section>
-                        :
-                        <div className='flex items-center justify-center w-full h-screen'>
+                {
+                    show &&
+                    <div className='z-30 absolute bg-white top-0 left-0 flex items-center justify-center w-full h-screen'>
                             <p className='text-lg font-pacific font-bold'>Loading...</p>
                         </div>
                 }
